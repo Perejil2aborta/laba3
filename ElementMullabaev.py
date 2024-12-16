5a0f338a5db181d602a20f475a20e4fe2ce2ff98
Perejil2aborta <danil.topilin.95@gmail.com>
2024-12-16 05:12:56 +0500

    Update ElementMullabaev.py

--- ElementMullabaev.py
+++ ElementMullabaev.py
@@ -1,10 +1,11 @@
 class ElementФамилия:
-    def init(self, name, symbol, number):
+    def __init__(self, name, symbol, number):
         self.__name = name
         self.__symbol = symbol
         self.__number = number
 
     def dump(self):
         print(f"Элемент: {self.__name}, Символ: {self.__symbol}, Номер: {self.__number}")
-aluminum = ElementФамилия("Алюминий", "Al", 13)
-vanadium.dump()
+aluminum = ElementФамилия("Алюминий", "Al",13)
+
+aluminum.dump()
